0b2ee9d2db8167816f42d9c7a827fe5834b60fb9
n_cho <n_cho@yumemi.co.jp>
2015-09-28 11:43:54 +0900

    Initial Commit

---
+++ PrivateCustom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrivateCustom
+//
+//  Created by n_cho on 9/28/15.
+//  Copyright © 2015 nic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
